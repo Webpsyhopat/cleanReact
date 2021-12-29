c08d70540a327e82d58f77b0cd4c686ac1a72d62
Dimm Vozhakoff <dimm.gd@gmail.com>
2021-12-29 22:54:37 +0200

    Feat. initialize project, add and configure babel and webpack

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+
+
+const App = () => {
+
+
+  return(
+    <div className='App'>
+      <h1>LOL!!! It works!</h1>
+    </div>
+  )
+}
+
+export default App
+
